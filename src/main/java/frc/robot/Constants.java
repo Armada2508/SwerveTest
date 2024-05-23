f3e21531d8fe9de200862833d69f2ea400ffe101
chris <christopp1228@gmail.com>
2024-05-23 16:29:51 -0500

    temp config file, constructor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,19 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+
 public class Constants {
     public static class SwerveK {
-    
+        public static int driveMotorID = 0;
+        public static int turnMotorID = 1;
+        public static int encoderID = 2;
+
+        public static Measure<Distance> wheelDiameter = Inches.of(3);
+
+        public static double steerGearRatio = 12.8;
+        public static double driveGearRatio = 6;
     }
 }
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems;
 
-public class Swerve {
+import java.io.File;
+import java.io.IOException;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveK;
+import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
+import swervelib.parser.SwerveParser;
+
+public class Swerve extends SubsystemBase {
+
+    SwerveDrive swerveDrive;
+    double maximumSpeed = Units.feetToMeters(4.5);
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+
+
+    public Swerve(File directory) throws IOException {
+        double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(SwerveK.steerGearRatio);
+        double driveConversionFactor = SwerveMath.calculateMetersPerRotation(SwerveK.driveGearRatio, SwerveK.driveGearRatio);
+
+
+        swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+    }
 }
