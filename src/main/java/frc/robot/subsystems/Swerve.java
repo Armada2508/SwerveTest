b8621e1ca00b8204edea7caec31b2a0840fd3428
chris <christopp1228@gmail.com>
2024-05-28 16:24:16 -0500

    verbosity, start drive code

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,28 +1,49 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Inches;
+
 import java.io.File;
 import java.io.IOException;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SwerveK;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class Swerve extends SubsystemBase {
 
     SwerveDrive swerveDrive;
     double maximumSpeed = Units.feetToMeters(4.5);
-    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
 
-
+    /**
+     * 
+     * @param directory Directory of swerve drive config files.
+     */
     public Swerve(File directory) throws IOException {
         double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(SwerveK.steerGearRatio);
-        double driveConversionFactor = SwerveMath.calculateMetersPerRotation(SwerveK.driveGearRatio, SwerveK.driveGearRatio);
+        double driveConversionFactor = SwerveMath.calculateMetersPerRotation(SwerveK.driveGearRatio, SwerveK.wheelDiameter.in(Inches));
 
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
         swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+    }
+
+    public void setupPathPlanner() {
+
+    }
+
+    public Command driveCommand() {
+        return runOnce(null); //! Fix
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        swerveDrive.drive(translation, rotation, fieldRelative, isOpenLoop);
     }
 }
