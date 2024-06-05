77f8a53b5d4ca2b0d454b8a0fd6fa1ee5929a591
chris <christopp1228@gmail.com>
2024-06-05 13:48:51 -0500

    drive commands, start path planning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,29 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import com.pathplanner.lib.util.PIDConstants;
 
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 
 public class Constants {
     public static class SwerveK {
-        public static int driveMotorID = 0;
-        public static int turnMotorID = 1;
-        public static int encoderID = 2;
+        public static final int driveMotorID = 0;
+        public static final int turnMotorID = 1;
+        public static final int encoderID = 2;
 
-        public static Measure<Distance> wheelDiameter = Inches.of(3);
+        public static final Measure<Distance> wheelDiameter = Inches.of(3); 
+        public static final Measure<Distance> driveBaseRadius = Meters.of(0.4579874);
 
-        public static double steerGearRatio = 12.8;
-        public static double driveGearRatio = 6;
+        public static final double steerGearRatio = 12.8;
+        public static final double driveGearRatio = 6;
+
+        public static final double maxModuleSpeed = 0; //! Tune
+
+        public static final PIDConstants translationConstants = new PIDConstants(1, 1, 1); //! Tune
+        public static final PIDConstants rotationConstants = new PIDConstants(1, 1, 1); //! Tune
+
     }
 }
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,13 +1,18 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
 
 import java.io.File;
 import java.io.IOException;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SwerveK;
@@ -32,18 +37,60 @@ public class Swerve extends SubsystemBase {
 
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
-        swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+        swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
     }
 
+    /**   
+     * 
+     * @param 
+     */
     public void setupPathPlanner() {
-
+        AutoBuilder.configureHolonomic(
+            this::getPose, 
+            this::resetOdometry, 
+            this::getRobotVelocity, 
+            this::setChassisSpeeds, 
+            new HolonomicPathFollowerConfig(SwerveK.translationConstants, 
+                                            SwerveK.rotationConstants, 
+                                            SwerveK.maxModuleSpeed, 
+                                            SwerveK.driveBaseRadius.in(Meters), 
+                                            null), 
+            null,
+            this);
+            
     }
 
-    public Command driveCommand() {
-        return runOnce(null); //! Fix
+    /**
+     * 
+     * @param TranslationX Translation in the X direction
+     * @param TranslationY Translation in the Y direction
+     * @param angularVelocity Angular Velocity to set 
+     * @return
+     */
+    public Command driveCommand(double TranslationX, double TranslationY, double angularVelocity) {
+        return runOnce(() -> drive(
+                new Translation2d(TranslationX * swerveDrive.getMaximumVelocity(), TranslationY * swerveDrive.getMaximumVelocity()), 
+                angularVelocity * swerveDrive.getMaximumAngularVelocity(), 
+                true, false));
     }
 
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
         swerveDrive.drive(translation, rotation, fieldRelative, isOpenLoop);
+    }
+
+    public void resetOdometry(Pose2d initialHolonomicPose) {
+        swerveDrive.resetOdometry(initialHolonomicPose);
+    }
+
+    public Pose2d getPose() {
+        return swerveDrive.getPose();
+    }
+
+    public ChassisSpeeds getRobotVelocity() {
+        return swerveDrive.getRobotVelocity();
+    }
+
+    public void setChassisSpeeds(ChassisSpeeds chassisSpeeds) {
+        swerveDrive.setChassisSpeeds(chassisSpeeds);
     }
 }
