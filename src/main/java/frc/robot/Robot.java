ad7b242a3539617080d0bd724b56d3b9643e18c4
chris <christopp1228@gmail.com>
2024-09-19 16:06:05 -0500

    add robot init and periodic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -49,6 +50,16 @@ public class Robot extends TimedRobot {
     
     private void configureBindings() {
         
+    }
+
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void robotPeriodic() {
+        System.out.println("Left X: " + xboxController.getLeftX() + " | Left Y" + xboxController.getLeftY());
     }
 
 }
