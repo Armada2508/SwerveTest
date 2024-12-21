d302d0fcbff347a84358f13c14fc7cf6934fb446
WispySparks <wispysparks@gmail.com>
2024-12-20 21:01:43 -0600

    update pathplanner, fix imports and path planner method

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,16 +1,21 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
 
 import java.io.File;
 
-import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.Filesystem;
 
 public class Constants {
@@ -26,6 +31,8 @@ public class Constants {
 
         public static final PIDConstants translationConstants = new PIDConstants(1, 1, 1); //! TODO: Tune
         public static final PIDConstants rotationConstants = new PIDConstants(1, 1, 1); //! TODO: Tune
+        public static final ModuleConfig moduleConfig = new ModuleConfig(wheelDiameter.div(2), maxRobotSpeed, 1, null, Amps.of(40), 1); //! TODO: Tune
+        public static final RobotConfig robotConfig = new RobotConfig(Pounds.of(0), KilogramSquareMeters.of(0), moduleConfig, new Translation2d[4]); //! TODO: Tune
 
         public static final File swerveDirectory = new File(Filesystem.getDeployDirectory().getAbsolutePath() + "/swerve");
     }
